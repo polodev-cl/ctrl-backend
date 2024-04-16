6e91374db593ed0524b02ef04d2ec22338c54c53
David Misael Villegas Sandoval <david.misa97@gmail.com>
2024-04-16 10:07:18 -0400

    Adjust default database name value in app.module.ts
    In the app.module.ts file, the naming of the database default value has been corrected. This change ensures that if the environment variable DB_DATABASE is not set, 'postgres' will be used as a default value.

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { AppService }      from './app.service';
           port: parseInt(process.env.DB_PORT, 10) || 5432,
           username: process.env.DB_USERNAME || 'postgres',
           password: process.env.DB_PASSWORD || 'postgres',
-          name: process.env.DB_NAME,
+          name: process.env.DB_DATABASE || 'postgres',
         },
         environment: process.env.NODE_ENV,
       }) ],
