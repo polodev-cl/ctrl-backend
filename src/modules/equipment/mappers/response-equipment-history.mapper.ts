4a60cc80d7b295456b7e6aa7044518ec437ef806
Diego Jara <diegomedina99j@gmail.com>
2024-07-12 19:01:51 -0400

    status mapper

--- src/modules/equipment/mappers/response-equipment-history.mapper.ts
+++ src/modules/equipment/mappers/response-equipment-history.mapper.ts
@@ -1,6 +1,7 @@
-import { UserEntity }             from '@modules/user/entities/user.entity';
-import { IEquipmentHistory }      from '@modules/equipment/interfaces/equipment-history.interface';
+import { UserEntity } from '@modules/user/entities/user.entity';
+import { IEquipmentHistory } from '@modules/equipment/interfaces/equipment-history.interface';
 import { EquipmentHistoryEntity } from '@modules/equipment/entities/equipment-history.entity';
+import { EquipmentStatusEnum } from '../enums/equipment-status.enum';
 
 export class ResponseEquipmentHistoryMapper implements Partial<IEquipmentHistory> {
   public readonly id: number;
@@ -16,16 +17,33 @@ export class ResponseEquipmentHistoryMapper implements Partial<IEquipmentHistory
   static map(entity: EquipmentHistoryEntity): ResponseEquipmentHistoryMapper {
     let usuario: Partial<UserEntity>;
 
-    if (entity.usuario) usuario = {
-      id: entity.usuario.id,
-      nombres: entity.usuario.nombres + ' ' + entity.usuario.apellidos,
-      rut: entity.usuario.rut
-    };
+    if (entity.usuario) {
+      usuario = {
+        id: entity.usuario.id,
+        nombres: entity.usuario.nombres + ' ' + entity.usuario.apellidos,
+        rut: entity.usuario.rut
+      };
+    }
+
+    // status mapper
+
+    let descripcion = entity.descripcion;
+
+    const estadoCambioRegex = /estado cambió de (\d+) a (\d+)/;
+    const match = estadoCambioRegex.exec(descripcion);
+
+    if (match) {
+      const estadoAnterior = parseInt(match[1], 10);
+      const estadoNuevo = parseInt(match[2], 10);
+      const estadoAnteriorTraducido = EquipmentStatusEnum[estadoAnterior] || match[1];
+      const estadoNuevoTraducido = EquipmentStatusEnum[estadoNuevo] || match[2];
+      descripcion = descripcion.replace(match[0], `estado cambió de ${estadoAnteriorTraducido} a ${estadoNuevoTraducido}`);
+    }
 
     return new ResponseEquipmentHistoryMapper({
       id: entity.id,
       equipoId: entity.equipoId,
-      descripcion: entity.descripcion,
+      descripcion: descripcion,
       fechaCreacion: entity.fechaCreacion,
       usuario: usuario
     });
